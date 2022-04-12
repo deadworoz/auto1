febb1999c66577119b2992ba4ea1d3c2614ec10d
deadworoz <stanislav.varenkov@skyeng.ru>
2022-04-12 14:50:33 +0300

    Install bundle for api doc

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
